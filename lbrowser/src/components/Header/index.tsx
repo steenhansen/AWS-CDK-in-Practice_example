7506deeed27a4e7102d6ecb04d013c020383f046
unknown <steen_hansen@yahoo.com>
2024-08-23 17:51:31 -0700

    Update index.tsx

--- lbrowser/src/components/Header/index.tsx
+++ lbrowser/src/components/Header/index.tsx
@@ -14,7 +14,7 @@ export const Header: React.FC = () => {
   return (
     <HeaderContainer>
       <div className="brand">
-        <a href="/" data-testid={"header-logo"}>{text_logo}</a> yo ho
+        <a href="/" data-testid={"header-logo"}>{text_logo}</a> yo ho ho
       </div>
     </HeaderContainer>
   );
