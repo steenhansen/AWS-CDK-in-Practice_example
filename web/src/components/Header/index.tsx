00eb9189f73cb32ff33ef27c3e91de96379cffea
unknown <steen_hansen@yahoo.com>
2024-08-28 00:40:35 -0700

    Update index.tsx

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
   return (
     <HeaderContainer>
       <div className="brand">
-        <a href="/" data-testid={"header-logo"}>Westpoint</a>
+        <a href="/" data-testid={"header-logo"}>Westpoint</a> bart
       </div>
     </HeaderContainer>
   );
