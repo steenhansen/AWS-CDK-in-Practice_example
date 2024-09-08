e9bea41b36a2dc62360e685b9753d02417904323
unknown <steen_hansen@yahoo.com>
2024-09-08 16:29:42 -0700

    Update api.test.ts

--- server/src/__tests__/api.test.ts
+++ server/src/__tests__/api.test.ts
@@ -3,52 +3,61 @@ import the_constants from '../../../cdk/program.constants.json';
 const HEALTH_CHECK_OK = the_constants.HEALTH_CHECK_OK;
 import supertest from 'supertest';
 import { describe, expect, test, it, afterAll } from '@jest/globals';
-
-import { DynamoDB } from 'aws-sdk';  // need this for Back-End-Test as no dynamo_layers in test
-
 const HEALTH_CHECK_SLUG = the_constants.HEALTH_CHECK_SLUG;
 const health_check = "/" + HEALTH_CHECK_SLUG;
 
-const PORT_TEST = 3007;  //constants_config.PORT_TEST;
 
-let server: any;
-const app = create_app();
-try {
-  server = app.listen(PORT_TEST, () => {
-    //console.info(`Test server is listening on port ${PORT_TEST}`);
-  });
-  //throw "Error: listen EACCES: permission denied 0.0.0.0:3003";
-} catch (e) {
-  const my_arr = [e,
-    "If above error is 'Error: listen EACCES: permission denied 0.0.0.0:3003' ",
-    "   and on Windows then clear stuck ports with",
-    "net stop winnat",
-    "net start winnat"];
-  console.log(my_arr);
+
+import express from 'express';
+import cors from 'cors';
+import { healthcheck_handler } from '../../../cdk/lib/constructs/Lambda/healthcheck/routine/';
+
+function corsResponse(the_response: string) {
+  const cors_resp = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET"
+    },
+    body: JSON.stringify({ message: the_response }),
+  };
+  return cors_resp;
 }
 
-server.keepAliveTimeout = 10;
-server.headersTimeout = 10;
-create_app();
 
+const createApp = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  const health_check = "/" + HEALTH_CHECK_SLUG;
+  app.get(health_check, async (_req, res) => {
+    try {
+      const response = await healthcheck_handler();
+      const response_json = JSON.stringify(response);
+      return res.status(200).send(response_json);
+    } catch (e: unknown) {
+      return corsResponse("the_e");
+    }
+  });
+
+
+
+  return app;
+};
+
+
+
+const app = createApp();
 describe('health check route', () => {
   it('Should return status 200 and text: "${HEALTH_CHECK_OK}"', async () => {
     const super_test = await supertest(app).get(health_check);  //'/health');
     const { statusCode, text } = super_test;
     const the_text = JSON.parse(text);
     expect(statusCode).toBe(200);
-
     const health_check_ok = `"${HEALTH_CHECK_OK}"`;
-    //    expect(the_text.body).toEqual(`"${HEALTH_CHECK_OK}"`);
     expect(the_text.body).toEqual(health_check_ok);
   });
-});
-
-// SNAP SHOT?????
-
-
-
-afterAll(async () => {
-  server.close();
 });
 
