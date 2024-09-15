5d2d3167f45fd7a50b3b6b3ae3ce0b91b0b5fbaa
unknown <steen_hansen@yahoo.com>
2024-09-15 16:13:16 -0700

    typescript resolution error

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import cdk_config from '../../../../cicd/cdk.json';
-import program_config from '../../../../cicd/program.config.json';
+//import cdk_config from '../../../../cicd/cdk.json';
+//import program_config from '../../../../cicd/program.config.json';
 import { SidebarContainer } from './styles';
 
-const WORK_ENV = cdk_config.context.global_consts.WORK_ENV;
-const ENVIRON_PRODUCTION = program_config.ENVIRON_PRODUCTION;
-const ENVIRON_DEVELOP = program_config.ENVIRON_DEVELOP;
+// const WORK_ENV = cdk_config.context.global_consts.WORK_ENV;
+// const ENVIRON_PRODUCTION = program_config.ENVIRON_PRODUCTION;
+// const ENVIRON_DEVELOP = program_config.ENVIRON_DEVELOP;
 
 let program_type = "";
-if (WORK_ENV === ENVIRON_PRODUCTION) {
-  program_type = "Production";
-} else if (WORK_ENV === ENVIRON_DEVELOP) {
-  program_type = "Development";
-}
+// if (WORK_ENV === ENVIRON_PRODUCTION) {
+//   program_type = "Production";
+// } else if (WORK_ENV === ENVIRON_DEVELOP) {
+//   program_type = "Development";
+// }
 
 
 export const Sidebar: React.FC = () => {
