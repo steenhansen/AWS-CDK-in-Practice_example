7fc3d0c3e3436bc9f34b13af4dc4bb3f0b25fb75
unknown <steen_hansen@yahoo.com>
2024-09-17 00:34:45 -0700

    Update server.cdk.test.ts

--- server/src/__tests__/server.cdk.test.ts
+++ server/src/__tests__/server.cdk.test.ts
@@ -1,34 +1,50 @@
-const supertest = require("supertest");
+// const supertest = require("supertest");
+// const { healthApp } = require('../health-app.js'); /// .JS
+// const { HEALTH_CHECK_OK, HEALTH_CHECK_SLUG } = require('../../../cicd/program.constants.json');
+// const { TESTING_ALIVE } = require('.../../../cicd/program.config.json');
+// const health_check = "/" + HEALTH_CHECK_SLUG;
 
+// if (TESTING_ALIVE === 'yes') {
 
-const { healthApp } = require('../health-app.js'); /// .JS
+//   // this checks the installed but ignored /server on aws
 
+//   const app = healthApp();
+//   describe('health check route', () => {
+//     it(`Should return status 200 and text: "${HEALTH_CHECK_OK}"`, async () => {
+//       const super_test = await supertest(app).get(health_check);
+//       const { statusCode, text } = super_test;
+//       const the_text = JSON.parse(text);
+//       expect(statusCode).toBe(200);
+//       const health_check_ok = `"${HEALTH_CHECK_OK}"`;
+//       expect(the_text.body).toEqual(health_check_ok);
+//     });
+//   });
 
-const { HEALTH_CHECK_OK, HEALTH_CHECK_SLUG } = require('../../../cicd/program.constants.json');
-const { TESTING_ALIVE } = require('.../../../cicd/program.config.json');
-const health_check = "/" + HEALTH_CHECK_SLUG;
+// } else {
 
-if (TESTING_ALIVE === 'yes') {
+//   it('server-at-least-one-cdk-test', () => {
+//     expect("server-at-least-one-cdk-test").toBe("server-at-least-one-cdk-test");
+//   });
 
-  // this checks the installed but ignored /server on aws
+// }
 
-  const app = healthApp();
-  describe('health check route', () => {
-    it(`Should return status 200 and text: "${HEALTH_CHECK_OK}"`, async () => {
-      const super_test = await supertest(app).get(health_check);
-      const { statusCode, text } = super_test;
-      const the_text = JSON.parse(text);
-      expect(statusCode).toBe(200);
-      const health_check_ok = `"${HEALTH_CHECK_OK}"`;
-      expect(the_text.body).toEqual(health_check_ok);
-    });
+import { healthApp } from '../health-app';
+
+import supertest from 'supertest';
+
+const app = healthApp();
+
+const healthPathResult = {
+  status: 'available',
+};
+
+describe('health check route', () => {
+  it('Should return status 200 and message: {status: "available"}', async () => {
+    const { text, statusCode } = await supertest(app).get('/healthCheck');
+    const the_text = JSON.parse(text);
+    expect(statusCode).toBe(200);
+    //expect(body).toEqual(healthPathResult);
+    expect(the_text.body).toEqual(`"Health-OK"`);
   });
-
-} else {
-
-  it('server-at-least-one-cdk-test', () => {
-    expect("server-at-least-one-cdk-test").toBe("server-at-least-one-cdk-test");
-  });
-
-}
+});
 
