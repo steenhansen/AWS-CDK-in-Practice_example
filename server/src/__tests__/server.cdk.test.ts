0a0ec24c3b0c849bd579ab9213f5603a5051be1d
unknown <steen_hansen@yahoo.com>
2024-09-17 11:02:50 -0700

    ...

--- server/src/__tests__/server.cdk.test.ts
+++ server/src/__tests__/server.cdk.test.ts
@@ -27,19 +27,34 @@
 //   });
 
 // }
-
+import { describe, expect, it } from '@jest/globals';
 import healthApp from '../health-app2';
 
 import supertest from 'supertest';
+const { HEALTH_CHECK_OK, HEALTH_CHECK_SLUG } = require('../../../cicd/program.constants.json');
+const { TESTING_ALIVE } = require('.../../../cicd/program.config.json');
+const health_check = "/" + HEALTH_CHECK_SLUG;
 
 const app = healthApp();
 
-describe('health check route', () => {
-  it('Should return status 200 and message: {status: "available"}', async () => {
-    const { text, statusCode } = await supertest(app).get('/healthCheck');
-    const the_text = JSON.parse(text);
-    expect(statusCode).toBe(200);
-    expect(the_text.body).toEqual(`"Health-OK"`);
+if (TESTING_ALIVE === 'yes') {
+
+  const health_check_ok = `"${HEALTH_CHECK_OK}"`;
+
+  describe('health check route', () => {
+    it('Should return status 200 and message: {status: "available"}', async () => {
+      const { text, statusCode } = await supertest(app).get(health_check);
+      const the_text = JSON.parse(text);
+      expect(statusCode).toBe(200);
+      expect(the_text.body).toEqual(health_check_ok);
+    });
   });
-});
+
+} else {
+
+  it('server-at-least-one-cdk-test', () => {
+    expect("server-at-least-one-cdk-test").toBe("server-at-least-one-cdk-test");
+  });
+
+}
 
--- web/src/tests/web.cdk.test.tsx
+++ web/src/tests/web.cdk.test.tsx
@@ -1,18 +1,13 @@
-//import '@testing-library/jest-dom';
-
-
 import React from 'react';         // NB if not present  -    ReferenceError: React is not defined
 import { App } from '../components/App';
 import { render, screen } from '@testing-library/react';
-
-
+import { expect, it } from '@jest/globals';
+import '@testing-library/jest-dom';
 
 const { TESTING_ALIVE } = require('../../program.pipeline.json');
 
 // in pipeline's Front-End-Test
-
 // cannot snapshot test in pipeline tests gitignore to ensure
-
 // there are no lambda functions yet to test db stuff
 
 if (TESTING_ALIVE === 'yes') {
