45649431d69da23e9e351dd8ef28b86881f8a3ab
unknown <steen_hansen@yahoo.com>
2024-09-17 11:50:11 -0700

    ...

--- web/src/tests/web.cdk.test.tsx
+++ web/src/tests/web.cdk.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';         // NB if not present  -    ReferenceError: React is not defined
 import { App } from '../components/App';
-import { render, screen } from '@testing-library/react';
-import { expect, it } from '@jest/globals';
+//import { render, screen } from '@testing-library/react';
+//import { expect, it } from '@jest/globals';
 import '@testing-library/jest-dom';
+//import '@testing-library/jest-dom/extend-expect';
 
 const { TESTING_ALIVE } = require('../../program.pipeline.json');
+
+
+import { render, screen, waitFor } from '@testing-library/react';
+
 
 // in pipeline's Front-End-Test
 // cannot snapshot test in pipeline tests gitignore to ensure
@@ -12,10 +17,12 @@ const { TESTING_ALIVE } = require('../../program.pipeline.json');
 
 if (TESTING_ALIVE === 'yes') {
 
-  test('renders the  logo', () => {
+  test('renders the  logo', async () => {
     render(<App />);
-    const westpointLogo = screen.getByTestId('header-logo');
-    expect(westpointLogo).toBeInTheDocument();
+    // const westpointLogo = screen.getByTestId('header-logo');
+    await waitFor(() => {
+      expect(screen.getByTestId('header-logo')).toBeInTheDocument();
+    });
   });
 
 } else {
