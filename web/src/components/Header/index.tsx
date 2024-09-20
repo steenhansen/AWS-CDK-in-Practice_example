1fd0645de55f66022c45061638ea63107f9f35f0
unknown <steen_hansen@yahoo.com>
2024-09-20 13:02:42 -0700

    ...

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
   return (
     <HeaderContainer>
       <div className="brand">
-        <a href="/" data-testid={"header-logo"}>DynamoDB colors via Lambdas ... 234</a>
+        <a href="/" data-testid={"header-logo"}>DynamoDB colors via Lambdas ... 234 789</a>
       </div>
     </HeaderContainer>
   );
